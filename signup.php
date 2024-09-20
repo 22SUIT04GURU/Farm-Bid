b1e608071f7816994f5859f0e0ddfdca3aa0b26f
vijay <22suit17@tcarts.in>
2024-09-20 23:00:56 +0530

    changes

--- signup.php
+++ signup.php
@@ -335,7 +335,7 @@
 </html>
 <style type="text/css">
   *
-  {
+{
     font-family: "Roboto",  system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
 }
 </style>
