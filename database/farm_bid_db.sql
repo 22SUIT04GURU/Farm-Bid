f488a69348c5b1009e8b7cd16f4db64dffbd8a67
Bairavar <22suit04@tcarts.in>
2024-10-11 11:57:49 +0530

    Database Added

--- database/farm_bid_db.sql
+++ database/farm_bid_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 21, 2024 at 04:20 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- Generation Time: Oct 11, 2024 at 08:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `farm_bid_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auction_item_tbl`
+--
+
+CREATE TABLE `auction_item_tbl` (
+  `id` int(255) NOT NULL,
+  `farmer_id` int(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `item_category` varchar(255) NOT NULL,
+  `initial_amount` int(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `status` varchar(25) NOT NULL DEFAULT 'pending',
+  `start_time` varchar(255) NOT NULL DEFAULT 'requested...',
+  `end_time` varchar(255) NOT NULL DEFAULT 'requested...',
+  `highest_bidder` varchar(255) NOT NULL DEFAULT 'waiting for the time',
+  `highest_bidder_amount` bigint(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `auction_item_tbl`
+--
+
+INSERT INTO `auction_item_tbl` (`id`, `farmer_id`, `item_name`, `item_category`, `initial_amount`, `description`, `image`, `status`, `start_time`, `end_time`, `highest_bidder`, `highest_bidder_amount`, `created_at`) VALUES
+(1, 1, 'Product 1', 'Rice ', 1500, 'This is the RIce that having the 3ky', 'dark-night-mountains-minimalist-4k-o4-1910x1075.jpg', 'On_Going', '2024-10-11T11:45', '2024-10-11T12:00', '4', 7500, '2024-10-11 06:18:51'),
+(2, 2, 'Product 2', 'Rice ', 2500, 'This is the Rice PRoduct having 34kg green rice', 'dark-night-mountains-minimalist-4k-o4-1910x1075.jpg', 'On_Going', '2024-10-11T11:49', '2024-10-11T11:58', 'waiting for the time', 2500, '2024-10-11 06:19:53');
 
 -- --------------------------------------------------------
 
@@ -40,9 +70,8 @@ CREATE TABLE `dealers_tbl` (
 --
 
 INSERT INTO `dealers_tbl` (`id`, `user_id`, `shop_name`, `shop_location`, `type_of_crop_prefer_to_buy`) VALUES
-(1, 4, 'Davind Foundations', 'Madurai', 'Wheat'),
-(3, 7, 'Davind Foundations', 'Madurai', 'Millets'),
-(4, 8, 'Guru SHop', 'Madurai', 'Chiru dhaniyam');
+(1, 3, 'Dealer A', 'Madurai', 'Paddy'),
+(2, 4, 'Dealer B', 'Madurai', 'Millets');
 
 -- --------------------------------------------------------
 
@@ -70,7 +99,8 @@ CREATE TABLE `farmers_tbl` (
 --
 
 INSERT INTO `farmers_tbl` (`id`, `user_id`, `address`, `district`, `pincode`, `farm_name`, `farm_location`, `farm_size`, `type_of_crop`, `bank_name`, `ifsc_code`, `account_number`) VALUES
-(1, 6, '34, Sidhi Vinayagar street', 'Namakkal', '123456', 'My Farm', 'Madurai', '12', 'Wheat', 'State Bank of India', 'SBIN0001234', 12345678);
+(1, 1, '34, MLM Colony, madurai', 'Madurai', '625011', 'White Farms', 'Madurai', 13, 'Wheat', 'State Bank of India', 'SBIN0001234', 123456789),
+(2, 2, '34, MLM Colony, madurai', 'Madurai', '625011', 'White Farms', 'Madurai', 13, 'Paddy', 'State Bank of India', 'SBIN0001234', 1234567889);
 
 -- --------------------------------------------------------
 
@@ -96,14 +126,20 @@ CREATE TABLE `user_tbl` (
 --
 
 INSERT INTO `user_tbl` (`id`, `first_name`, `date_of_birth`, `email`, `phone`, `gender`, `role`, `username`, `password`, `registered_date`) VALUES
-(4, 'John David', '2024-09-01', 'Charan@gmail.com', 9842240830, 'male', 'dealer', 'Guru', 'guru@123', '2024-09-21 09:00:35'),
-(6, 'Gnana Guru', '2024-09-01', '22suit04@gmail.com', 9842240830, 'male', 'farmer', 'Guru-bairavar', 'guru@123', '2024-09-21 09:06:28'),
-(7, 'Ajay', '2024-08-30', 'jeyantah@gmail.com', 9842240830, 'male', 'dealer', 'Guru-bairavar-g', 'guru@123', '2024-09-21 09:08:51'),
-(8, 'Charan', '2024-09-01', 'Guru@gmail.com', 1234567678, 'male', 'dealer', 'GuruM', 'guru@123', '2024-09-21 10:06:20');
+(1, 'PersonA', '2024-10-02', 'persona@gmail.com', 9842240830, 'male', 'farmer', 'persona', 'guru@123', '2024-10-09 13:25:27'),
+(2, 'PersonB', '1212-12-12', 'gurubairavar005@gmail.com', 9842240830, 'male', 'farmer', 'personb', 'guru@123', '2024-10-09 13:27:13'),
+(3, 'dealera', '1212-12-12', 'gurubairavar005@gmail.com', 9842240830, 'male', 'dealer', 'dealera', 'guru@123', '2024-10-09 13:28:01'),
+(4, 'dealerb', '1212-03-31', 'gurubairavar005@gmail.com', 9842240830, 'male', 'dealer', 'dealerb', 'guru@123', '2024-10-09 13:28:52');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `auction_item_tbl`
+--
+ALTER TABLE `auction_item_tbl`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `dealers_tbl`
@@ -131,22 +167,28 @@ ALTER TABLE `user_tbl`
 --
 
 --
+-- AUTO_INCREMENT for table `auction_item_tbl`
+--
+ALTER TABLE `auction_item_tbl`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `dealers_tbl`
 --
 ALTER TABLE `dealers_tbl`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `farmers_tbl`
 --
 ALTER TABLE `farmers_tbl`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_tbl`
 --
 ALTER TABLE `user_tbl`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
